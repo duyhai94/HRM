8f3f1f275a8d07bd05e86b01b66ed22ff7732834
Duy Hai <cdh271294@gmail.com>
2021-01-05 14:59:15 +0700

    update profile-detail

--- src/app/pages/profile/component/profile-detail/profile-detail.page.ts
+++ src/app/pages/profile/component/profile-detail/profile-detail.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-detail.page.scss'],
 })
 export class ProfileDetailPage implements OnInit {
-
+  selected: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showInfo(){
+    this.selected = !this.selected;
+    
+  }
 }
